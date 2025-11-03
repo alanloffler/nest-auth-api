fe85143249e03aba33bcfb68f023be553a82bd1e
alanloffler <alanmatiasloffler@gmail.com>
2025-11-02 23:37:40 -0300

    feat(auth): import admin module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,11 @@
 import { Module } from "@nestjs/common";
 
+import { AdminModule } from "@/admin/admin.module";
 import { AuthController } from "@auth/auth.controller";
 import { AuthService } from "@auth/auth.service";
 
 @Module({
+  imports: [AdminModule],
   controllers: [AuthController],
   providers: [AuthService],
 })
