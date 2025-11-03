52ad15043758915fc9edfe043fb1c0b8a65e375d
alanloffler <alanmatiasloffler@gmail.com>
2025-11-03 00:31:18 -0300

    feat(auth): validate email and password

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,29 +1,43 @@
-import { Injectable } from "@nestjs/common";
+import type { Response } from "express";
+import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
 
+import type { IPayload } from "@auth/interfaces/payload.interface";
 import type { IRequest } from "@auth/interfaces/request.interface";
 import { AdminService } from "@/admin/admin.service";
+import { ApiResponse } from "@/common/helpers/api-response.helper";
 
 @Injectable()
 export class AuthService {
   constructor(private readonly adminService: AdminService) {}
 
-  async signIn(req: IRequest, res) {
-    console.log(req.user);
-    // const admin = await this.adminService.findOne({ where: { email } });
-    // return admin;
+  async signIn(req: IRequest, res: Response) {
+    // console.log(req.user);
+    // console.log(res);
+    // TODO: Implement token generation and send it as cookie to client in res
+    return ApiResponse.success("Usuario logueado", req.user);
   }
 
-  async validateUser(email: string, password: string): Promise<any> {
-    const user = await this.adminService.findOne({ where: { email } });
+  async validateUser(email: string): Promise<IPayload> {
+    const user = await this.adminService.findOneByEmail(email);
 
-    if (!user) {
-      throw new HttpException("Invalid credentials", HttpStatus.UNAUTHORIZED);
+    return {
+      id: user.id,
+      email: user.email,
+      role: user.role.value,
+    };
+  }
+
+  async validatePassword(email: string, password: string): Promise<IPayload> {
+    const user = await this.adminService.findOneByEmail(email);
+
+    if (!user || !user.password || user.password !== password) {
+      throw new HttpException("Contraseña incorrecta", HttpStatus.UNAUTHORIZED);
     }
 
     return {
       id: user.id,
       email: user.email,
-      role: user.role,
+      role: user.role.value,
     };
   }
 }
