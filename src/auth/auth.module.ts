61d466bf1a4ad6f79f3eb92c2c9ca5964a727c46
alanloffler <alanmatiasloffler@gmail.com>
2025-11-03 00:31:40 -0300

    feat(auth): local strategy provider

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,10 +3,11 @@ import { Module } from "@nestjs/common";
 import { AdminModule } from "@/admin/admin.module";
 import { AuthController } from "@auth/auth.controller";
 import { AuthService } from "@auth/auth.service";
+import { LocalStrategy } from "@auth/strategies/local.strategy";
 
 @Module({
   imports: [AdminModule],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, LocalStrategy],
 })
 export class AuthModule {}
