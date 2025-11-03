48cee0a733ee7cc9fd921c131c7cdf769fa390df
alanloffler <alanmatiasloffler@gmail.com>
2025-11-02 21:43:48 -0300

    feat(app): imports and configs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,32 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from "@nestjs/config";
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+
+import { AdminModule } from "@admin/admin.module";
+import { AuthModule } from "@auth/auth.module";
+import { RolesModule } from "@roles/roles.module";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ".env",
+    }),
+    TypeOrmModule.forRoot({
+      type: "postgres",
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT) || 5432,
+      username: process.env.DB_USERNAME,
+      password: String(process.env.DB_PASSWORD),
+      database: process.env.DB_DATABASE,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    AdminModule,
+    AuthModule,
+    RolesModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
