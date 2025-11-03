b5f8820740f58ac859b30dd164cde2c537db8cfc
alanloffler <alanmatiasloffler@gmail.com>
2025-11-02 23:37:56 -0300

    feat(auth): sign in function

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from "@nestjs/common";
+import type { Response } from "express";
+import { Controller, Post, Req, Res } from "@nestjs/common";
+
+import type { IRequest } from "@auth/interfaces/request.interface";
+import { AuthService } from "@auth/auth.service";
 
 @Controller("auth")
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post("signIn")
+  signIn(@Req() req: IRequest, @Res({ passthrough: true }) res: Response) {
+    return this.authService.signIn(req, res);
+  }
+}
