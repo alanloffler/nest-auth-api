000463520fb31f2800f132b2260c70ab785dd959
alanloffler <alanmatiasloffler@gmail.com>
2025-11-17 19:25:49 -0300

    remove(common): roles decorator

--- src/common/decorators/roles.decorator.ts
+++
@@ -1,7 +0,0 @@
-import { SetMetadata } from "@nestjs/common";
-
-import { ERole } from "@common/enums/role.enum";
-
-export const ROLES_KEY: string = "roles";
-
-export const Roles = (roles: ERole[] | null = []) => SetMetadata(ROLES_KEY, roles);
