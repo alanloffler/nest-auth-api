bd3e00e2604ea67758d783586f0c7cd1d843a7c0
alanloffler <alanmatiasloffler@gmail.com>
2025-11-17 19:26:52 -0300

    feat(admin): implement roles guard

--- src/admin/admin.controller.ts
+++ src/admin/admin.controller.ts
@@ -1,13 +1,15 @@
 import { Body, Controller, Delete, Get, Param, ParseUUIDPipe, Patch, Post, UseGuards } from "@nestjs/common";
 
 import { AdminService } from "@admin/admin.service";
-import { CreateAdminDto } from "@/admin/dto/create-admin.dto";
-import { ERole } from "@/common/enums/role.enum";
-import { JwtAuthGuard } from "@/auth/guards/jwt-auth.guard";
-import { Roles } from "@/common/decorators/roles.decorator";
+import { CreateAdminDto } from "@admin/dto/create-admin.dto";
+import { ERole } from "@common/enums/role.enum";
+import { JwtAuthGuard } from "@auth/guards/jwt-auth.guard";
+import { Roles } from "@auth/decorators/roles.decorator";
+import { RolesGuard } from "@auth/guards/roles.guard";
 import { UpdateAdminDto } from "@admin/dto/update-admin.dto";
 
-@Roles([ERole.Superadmin])
+@UseGuards(JwtAuthGuard, RolesGuard)
+@Roles([ERole.Superadmin, ERole.Admin])
 @Controller("admin")
 export class AdminController {
   constructor(private readonly adminService: AdminService) {}
@@ -17,17 +19,18 @@ export class AdminController {
     return this.adminService.create(admin);
   }
 
-  @UseGuards(JwtAuthGuard)
   @Get()
   findAll() {
     return this.adminService.findAll();
   }
 
+  @Roles([ERole.Teacher])
   @Get(":id")
   findOne(@Param("id", ParseUUIDPipe) id: string) {
     return this.adminService.findOne(id);
   }
 
+  @Roles([ERole.Teacher])
   @Patch(":id")
   update(@Param("id", ParseUUIDPipe) id: string, @Body() admin: UpdateAdminDto) {
     return this.adminService.update(id, admin);
