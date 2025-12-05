d5dcd8fa526233eed33a3485b1197b95c31e5b26
alanloffler <alanmatiasloffler@gmail.com>
2025-12-05 14:48:21 -0300

    refactor(app): typeorm data source

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 import { AdminModule } from "@admin/admin.module";
 import { AuthModule } from "@auth/auth.module";
 import { RolesModule } from "@roles/roles.module";
+import { typeOrmConfig } from "@config/typeorm.config";
 
 @Module({
   imports: [
@@ -13,14 +14,8 @@ import { RolesModule } from "@roles/roles.module";
       envFilePath: ".env",
     }),
     TypeOrmModule.forRoot({
-      type: "postgres",
-      host: process.env.DB_HOST,
-      port: Number(process.env.DB_PORT) || 5432,
-      username: process.env.DB_USERNAME,
-      password: String(process.env.DB_PASSWORD),
-      database: process.env.DB_DATABASE,
+      ...typeOrmConfig,
       autoLoadEntities: true,
-      synchronize: true,
     }),
     AdminModule,
     AuthModule,
