3e625507961cfcf51ae230a151384284e3c6a4a7
alanloffler <alanmatiasloffler@gmail.com>
2025-12-05 14:48:43 -0300

    feat(typeorm): data source

--- typeorm.datasource.ts
+++ typeorm.datasource.ts
@@ -1,14 +1,3 @@
-import { DataSource } from "typeorm";
+import { AppDataSource } from "@config/typeorm.config";
 
-export default new DataSource({
-  type: "postgres",
-  host: "localhost",
-  port: 5432,
-  username: "postgres",
-  password: "admin1234",
-  database: "school_ems",
-  entities: ["src/**/*.entity.ts"],
-  migrations: ["migrations/*.ts"],
-  migrationsTableName: "migrations",
-  synchronize: false,
-});
+export default AppDataSource;
