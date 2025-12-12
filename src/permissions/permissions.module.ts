38164d0c6398725ede71a58f28b8f32176b3d9f5
alanloffler <alanmatiasloffler@gmail.com>
2025-12-12 19:33:22 -0300

    feat(permissions): cache module

--- src/permissions/permissions.module.ts
+++ src/permissions/permissions.module.ts
@@ -1,13 +1,23 @@
+import { CacheModule } from "@nestjs/cache-manager";
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 
 import { Permission } from "@permissions/entities/permission.entity";
+import { PermissionsCacheService } from "@permissions/permissions-cache.service";
 import { PermissionsController } from "@permissions/permissions.controller";
 import { PermissionsService } from "@permissions/permissions.service";
+import { Role } from "@roles/entities/role.entity";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Permission])],
+  imports: [
+    CacheModule.register({
+      ttl: 0,
+      max: 1000,
+    }),
+    TypeOrmModule.forFeature([Permission, Role]),
+  ],
   controllers: [PermissionsController],
-  providers: [PermissionsService],
+  providers: [PermissionsCacheService, PermissionsService],
+  exports: [PermissionsCacheService],
 })
 export class PermissionsModule {}
