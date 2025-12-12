e2cec7b5d0d835731d92e923f97fa80fad47f4d5
alanloffler <alanmatiasloffler@gmail.com>
2025-12-12 19:16:28 -0300

    feat(auth): permissions guard

---
+++ src/auth/guards/permissions.guard.ts
@@ -0,0 +1,73 @@
+// permissions.guard.ts
+import { CACHE_MANAGER } from "@nestjs/cache-manager";
+import { Cache } from "cache-manager";
+import { CanActivate, ExecutionContext, HttpException, HttpStatus, Inject, Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Reflector } from "@nestjs/core";
+import { Repository } from "typeorm";
+
+import { PERMISSIONS_KEY } from "@auth/decorators/require-permissions.decorator";
+import { Role } from "@roles/entities/role.entity";
+
+@Injectable()
+export class PermissionsGuard implements CanActivate {
+  constructor(
+    @Inject(CACHE_MANAGER) private cacheManager: Cache,
+    @InjectRepository(Role) private roleRepository: Repository<Role>,
+    private reflector: Reflector,
+  ) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const requiredPermissions = this.reflector.getAllAndOverride<string[]>(PERMISSIONS_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    if (!requiredPermissions) {
+      return true;
+    }
+
+    const request = context.switchToHttp().getRequest();
+    const user = request.user;
+
+    if (!user || !user.roleId) {
+      throw new HttpException("No tienes permisos para acceder a este recurso", HttpStatus.FORBIDDEN);
+    }
+
+    const userPermissions = await this.getRolePermissions(user.roleId);
+
+    if (!userPermissions || userPermissions.length === 0) {
+      throw new HttpException("Tu rol no posee permisos asignados", HttpStatus.FORBIDDEN);
+    }
+
+    const hasPermissions = requiredPermissions.some((permission) => userPermissions.includes(permission));
+    if (!hasPermissions) {
+      throw new HttpException(`El usuario no posee el permiso necesario: ${requiredPermissions}`, HttpStatus.FORBIDDEN);
+    }
+
+    return hasPermissions;
+  }
+
+  private async getRolePermissions(roleId: string): Promise<string[]> {
+    const cacheKey = `role_permissions_${roleId}`;
+
+    let permissions: string[] = (await this.cacheManager.get(cacheKey)) as string[];
+
+    if (!permissions) {
+      const role = await this.roleRepository.findOne({
+        where: { id: roleId },
+        relations: ["rolePermissions", "rolePermissions"],
+      });
+
+      if (!role || !role.rolePermissions) {
+        return [];
+      }
+
+      permissions = role.rolePermissions.map((rp) => rp.permission.actionKey);
+
+      await this.cacheManager.set(cacheKey, permissions, 0);
+    }
+
+    return permissions;
+  }
+}
