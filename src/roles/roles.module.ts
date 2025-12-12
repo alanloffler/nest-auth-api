31212e299b9fadceb760af32b4e098573c8afb1e
alanloffler <alanmatiasloffler@gmail.com>
2025-12-12 19:35:18 -0300

    feat(roles): import permissions module

--- src/roles/roles.module.ts
+++ src/roles/roles.module.ts
@@ -2,13 +2,14 @@ import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 
 import { Permission } from "@permissions/entities/permission.entity";
+import { PermissionsModule } from "@permissions/permissions.module";
 import { Role } from "@roles/entities/role.entity";
 import { RolePermission } from "@roles/entities/role-permission.entity";
 import { RolesController } from "@roles/roles.controller";
 import { RolesService } from "@roles/roles.service";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Permission, Role, RolePermission])],
+  imports: [PermissionsModule, TypeOrmModule.forFeature([Permission, Role, RolePermission])],
   controllers: [RolesController],
   providers: [RolesService],
 })
