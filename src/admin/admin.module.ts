c079021d36315ffb227f26bf52b304fd2b8ab0c4
alanloffler <alanmatiasloffler@gmail.com>
2025-12-13 11:25:04 -0300

    feat(admin): import role entity

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -4,9 +4,10 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 import { Admin } from "@admin/entities/admin.entity";
 import { AdminController } from "@admin/admin.controller";
 import { AdminService } from "@admin/admin.service";
+import { Role } from "@roles/entities/role.entity";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Admin])],
+  imports: [TypeOrmModule.forFeature([Admin, Role])],
   providers: [AdminService],
   controllers: [AdminController],
   exports: [AdminService],
