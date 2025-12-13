e5327e9daa9a410d8dec0bc34f8d37af51a0a390
alanloffler <alanmatiasloffler@gmail.com>
2025-12-13 11:19:39 -0300

    feat(permissions): required permissions guard

--- src/permissions/permissions.controller.ts
+++ src/permissions/permissions.controller.ts
@@ -1,50 +1,49 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from "@nestjs/common";
+import { Controller, Get, Post, Body, Patch, Param, UseGuards } from "@nestjs/common";
 
 import { CreatePermissionDto } from "@permissions/dto/create-permission.dto";
-import { ERole } from "@common/enums/role.enum";
 import { JwtAuthGuard } from "@auth/guards/jwt-auth.guard";
 import { PermissionsGuard } from "@auth/guards/permissions.guard";
 import { PermissionsService } from "@permissions/permissions.service";
-import { Roles } from "@auth/decorators/roles.decorator";
-import { RolesGuard } from "@auth/guards/roles.guard";
+import { RequiredPermissions } from "@auth/decorators/required-permissions.decorator";
 import { UpdatePermissionDto } from "@permissions/dto/update-permission.dto";
 
-@UseGuards(JwtAuthGuard, PermissionsGuard, RolesGuard)
+@UseGuards(JwtAuthGuard, PermissionsGuard)
 @Controller("permissions")
 export class PermissionsController {
   constructor(private readonly permissionsService: PermissionsService) {}
 
-  @Roles([ERole.Superadmin])
+  @RequiredPermissions("permissions-create")
   @Post()
   create(@Body() createPermissionDto: CreatePermissionDto) {
     return this.permissionsService.create(createPermissionDto);
   }
 
-  @Roles([ERole.Superadmin, ERole.Admin])
+  @RequiredPermissions("permissions-view")
   @Get()
   findAll() {
     return this.permissionsService.findAll();
   }
 
-  @Roles([ERole.Superadmin, ERole.Admin])
+  @RequiredPermissions("roles-update")
+  @RequiredPermissions("permissions-view")
   @Get("grouped")
   findAllGrouped() {
     return this.permissionsService.findAllGrouped();
   }
 
-  @Roles([ERole.Superadmin, ERole.Admin])
+  @RequiredPermissions("permissions-view")
   @Get(":id")
   findOne(@Param("id") id: string) {
     return this.permissionsService.findOne(id);
   }
 
-  @Roles([ERole.Superadmin, ERole.Admin])
+  @RequiredPermissions("permissions-update")
   @Patch(":id")
   update(@Param("id") id: string, @Body() updatePermissionDto: UpdatePermissionDto) {
     return this.permissionsService.update(id, updatePermissionDto);
   }
 
-  @Delete(":id")
+  @RequiredPermissions("permissions-delete")
   remove(@Param("id") id: string) {
     return this.permissionsService.remove(id);
   }
