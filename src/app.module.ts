1891f7f07eca9b5b1438e5f3dec06e36679f14fe
alanloffler <alanmatiasloffler@gmail.com>
2025-12-13 11:04:24 -0300

    feat(app): cache config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,14 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 
 import { AdminModule } from "@admin/admin.module";
 import { AuthModule } from "@auth/auth.module";
+import { CacheConfigModule } from "@config/cache-config.module";
 import { PermissionsModule } from "@permissions/permissions.module";
 import { RolesModule } from "@roles/roles.module";
 import { typeOrmConfig } from "@config/typeorm.config";
 
 @Module({
   imports: [
+    CacheConfigModule,
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: ".env",
