eaf65d1a1bceada81b34db0fc6c34fb9f7768d56
alanloffler <alanmatiasloffler@gmail.com>
2025-12-14 21:53:39 -0300

    feat(auth): password encripted compare

--- src/auth/strategies/local.strategy.ts
+++ src/auth/strategies/local.strategy.ts
@@ -1,13 +1,14 @@
-import { Injectable } from "@nestjs/common";
+import * as bcrypt from "bcrypt";
+import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
 import { PassportStrategy } from "@nestjs/passport";
 import { Strategy } from "passport-local";
 
 import type { IPayload } from "@auth/interfaces/payload.interface";
-import { AuthService } from "@auth/auth.service";
+import { AdminService } from "@admin/admin.service";
 
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy) {
-  constructor(private readonly authService: AuthService) {
+  constructor(private readonly adminService: AdminService) {
     super({
       usernameField: "email",
       passwordField: "password",
@@ -15,15 +16,20 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
   }
 
   async validate(email: string, password: string): Promise<IPayload> {
-    const user = await this.authService.validateUser(email);
+    const user = await this.adminService.findOneByEmail(email);
 
-    await this.authService.validatePassword(email, password);
+    if (!user) throw new HttpException("Credenciales inválidas", HttpStatus.UNAUTHORIZED);
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) throw new HttpException("Credenciales inválidas", HttpStatus.UNAUTHORIZED);
+
+    if (!user.role) throw new HttpException("El usuario posee un rol inactivo", HttpStatus.FORBIDDEN);
 
     return {
       id: user.id,
       email: user.email,
-      role: user.role,
-      roleId: user.roleId,
+      role: user.role.value,
+      roleId: user.role.id,
     };
   }
 }
