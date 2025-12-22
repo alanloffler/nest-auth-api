838c69f6c097d94d960da446d17d11fc75815b6a
alanloffler <alanmatiasloffler@gmail.com>
2025-12-22 19:48:51 -0300

    fix(main): domain path

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ["http://localhost:3000", "http://localhost:5173", "https://react-auth-reactive.vercel.app/"],
+    origin: ["http://localhost:3000", "http://localhost:5173", "https://react-auth-reactive.vercel.app"],
     credentials: true,
   });
   app.use(cookieParser());
